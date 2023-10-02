98ec988228c55dbee5139f7b3b41486b7b09f4db
Santiago <guzman.santiago135@gmail.com>
2023-10-02 10:58:42 -0300

    Modificado home

--- src/app/componentes/bienvenido/bienvenido.component.ts
+++ src/app/componentes/bienvenido/bienvenido.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/app/entidades/producto';
+import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
   selector: 'app-bienvenido',
   templateUrl: './bienvenido.component.html',
   styleUrls: ['./bienvenido.component.css']
 })
-export class BienvenidoComponent {
+export class BienvenidoComponent implements OnInit {
+  productos: Producto[] = [];
+
+  constructor(private productosService: ProductosService){}
+
+  ngOnInit(): void {
+    this.productosService.getProductos().subscribe(prods => this.productos = prods);
+    this.productos.slice(0, 3);
+  }
 
 }
