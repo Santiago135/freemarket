be1a2d24ca16f64344c4eb92101993891a961608
Santiago <guzman.santiago135@gmail.com>
2023-10-23 16:13:04 -0300

    Agregado pdf

--- src/app/componentes/carrito/carrito.component.ts
+++ src/app/componentes/carrito/carrito.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CarritoItem } from 'src/app/entidades/carrito-item';
 import { Producto } from 'src/app/entidades/producto';
 import { CarritoService } from 'src/app/services/carrito.service';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-carrito',
@@ -15,9 +17,11 @@ export class CarritoComponent {
   public showModal = false;
   public selectedItem: Producto = new Producto();
   public showModalDeleteItem = false;
+  public printingPDF = false;
 
   constructor(public carritoService: CarritoService) {
     this.UpdateCarritoPrice();
+    this.content = new ElementRef(null);
   }
 
   public AddItem(producto: Producto) {
@@ -25,7 +29,7 @@ export class CarritoComponent {
     this.UpdateCarritoPrice();
   }
 
-  public AskRemoveItem(producto: Producto){
+  public AskRemoveItem(producto: Producto) {
     this.selectedItem = producto;
     this.showModalDeleteItem = true;
   }
@@ -42,17 +46,66 @@ export class CarritoComponent {
     this.total = this.totales.reduce((total, valor) => total + valor, 0);
   }
 
-  public VaciarCarrito(){
+  public VaciarCarrito() {
     this.showModal = true;
   }
-  
-  public ClearCarrito(){
+
+  public ClearCarrito() {
     this.carritoService.ClearCart();
     location.reload();
   }
 
-  public closeModal(){
+  public closeModal() {
     this.showModal = false;
     this.showModalDeleteItem = false;
+  }
+
+  @ViewChild('contenido') content: ElementRef;
+
+  // exportToPDF() {
+  //   const data = this.content.nativeElement;
+  //   let date = new Date().toLocaleString();
+  //   let name = "factura-" + date
+  //   this.printingPDF = true
+  //   setTimeout(() => {
+  //     html2canvas(data).then(canvas => {
+  //       const imgWidth = 210;
+  //       const imgHeight = (canvas.height * imgWidth) / canvas.width;
+  //       const contentDataURL = canvas.toDataURL('image/png');
+  //       const pdf = new jsPDF('p', 'mm', 'a4');
+  //       pdf.addImage(contentDataURL, 'PNG', 0, 0, imgWidth, imgHeight);
+  //       pdf.save(name + ".pdf");
+  //     });
+  //     this.printingPDF = false;
+  //   }, 100)
+  // }
+
+  exportToPDF() {
+    const data = this.content.nativeElement;
+    let fecha = new Date();
+    let date = fecha.toLocaleString();
+    let name = "factura-" + date
+    this.printingPDF = true;
+    setTimeout(() => {
+      html2canvas(data).then(canvas => {
+        const imgWidth = 210;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+        const contentDataURL = canvas.toDataURL('image/png');
+        const pdf = new jsPDF('p', 'mm', 'a4');
+        pdf.addImage(contentDataURL, 'PNG', 0, 0, imgWidth, imgHeight);
+        pdf.setFontSize(10);
+
+        const texto1 = 'Gracias por confiar en nuestros productos/servicios. Su satisfacción es nuestra prioridad. Si necesita asistencia ';
+        const texto2 = "adicional o tiene alguna pregunta, no dude en ponerse en contacto con nosotros. Esperamos seguir siendo su elección ";
+        const texto3 = "preferida. ¡Gracias por su negocio! Compra efectuada el: " + date;
+        pdf.setTextColor(150, 150, 150);
+        pdf.text(texto1, 10, pdf.internal.pageSize.height - 15);
+        pdf.text(texto2, 10, pdf.internal.pageSize.height - 10);
+        pdf.text(texto3, 10, pdf.internal.pageSize.height - 5);
+        pdf.save(name + ".pdf");
+
+      });
+      this.printingPDF = false;
+    }, 5)
   }
 }
